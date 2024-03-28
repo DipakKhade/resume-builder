1a68022b39391d12ee74bbb8a89dfdd0ac99ac07
= <=>
2024-03-29 00:08:22 +0530

    done with landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 
 import './App.css'
+import {ThemeProvider} from './@/components/theme-provider'
+import Header from './@/components/Header'
+import { GlobeDemo } from './@/components/World'
+
 
 function App() {
 
   return (
    <>
+   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <Header/>
+   <GlobeDemo/>
+   </ThemeProvider>
    </>
   )
 }
