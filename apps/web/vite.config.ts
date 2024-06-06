e829871b99eb34eb9ea81685dfd42703d834b800
= <=>
2024-06-06 17:39:09 +0530

    deploymet v0

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    outDir: 'public', 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, 
+  },
 })
